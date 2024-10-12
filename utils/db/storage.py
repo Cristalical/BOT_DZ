1a8fea20096e2d0e818909ae355eca68c508ab21
Cristalical <ak3146753@gmail.com>
2024-10-12 18:42:03 +0700

    GOOD!????

--- utils/db/storage.py
+++ utils/db/storage.py
@@ -1,42 +1,88 @@
 import psycopg2 as p2
-from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
-try:
-    connection = p2.connect(user="postgres",
-                            password="qwerty",
-                            host="127.0.0.1",
-                            port="5432",
-                            database="dz_bot_bd")
-    # Создание изолированной среды для создания бд
-    """
-    connection.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
-    """
-    # Курсор отвечающий за выполнение операций с бд
-    cursor = connection.cursor()
-    # Создание бд
-    '''
-    sql_create_db = "CREATE DATABASE dz_bot_bd "
-    cursor.execute(sql_create_db)
-    '''
-    create_table_query = '''CREATE TABLE IF NOT EXISTS all_homework 
-                          (all_homework_id integer primary key NOT NULL,
-                          lessons text NOT NULL,
-                          description text NOT NULL,
-                          deadline date NOT NULL,
-                          format text NOT NULL,
-                          image_date bytea NOT NULL)'''
-    cursor.execute(create_table_query)
-    connection.commit()
-    print("Таблица была создана наверное)")
-    # Вывод сведений о базе данных
-    print("Информация о сервере базы данных...")
-    print(connection.get_dsn_parameters(), "\n")
-    cursor.execute("SELECT version();")
-    record = cursor.fetchone()
-    print("Вы подключены к - ", record, "\n")
-except (Exception, p2.Error) as error:
-    print("Ошибка при работе с базой данных:", error)
-finally:
-    if connection:
-        cursor.close()
-        connection.close()
-        print("Соединение с базой данных прервано...")
+
+class DatebaseManager:
+    def __init__(self, path):
+        self.conn = p2.connect(self, path)
+        self.conn.commit()
+        self.cur = self.conn.cursor()
+
+    def create_db(self):
+        self.query("CREATE DATABASE dz_bot_bd")
+
+    def create_table(self):
+        self.query('''CREATE TABLE IF NOT EXISTS all_homework 
+                    (all_homework_id integer primary key NOT NULL,
+                    lessons text NOT NULL,
+                    description text NOT NULL,
+                    deadline date NOT NULL,
+                    format text NOT NULL,
+                    image_date bytea NOT NULL)''')
+
+    def query(self, arg, values=None):
+        if values in None:
+            self.cur.execute(arg)
+        else:
+            self.cur.execute(arg, values)
+        self.conn.commit()
+
+    def fetchone(self, arg, values=None):
+        if values is None:
+            self.cur.execute(arg)
+        else:
+            self.cur.execute(arg, values)
+        return self.cur.fetchone()
+
+    def fetchall(self, arg, values=None):
+        if values is None:
+            self.cur.execute(arg)
+        else:
+            self.cur.execute(arg, values)
+        return self.cur.fetchall()
+
+    def __del__(self):
+        self.conn.close()
+
+
+
+
+
+# try:
+#     connection = p2.connect(user="postgres",
+#                             password="qwerty",
+#                             host="127.0.0.1",
+#                             port="5432",
+#                             database="dz_bot_bd")
+#     # Создание изолированной среды для создания бд
+#     """
+#     connection.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+#     """
+#     # Курсор отвечающий за выполнение операций с бд
+#     cursor = connection.cursor()
+#     # Создание бд
+#     '''
+#     sql_create_db = "CREATE DATABASE dz_bot_bd"
+#     cursor.execute(sql_create_db)
+#     '''
+#     create_table_query = '''CREATE TABLE IF NOT EXISTS all_homework
+#                           (all_homework_id integer primary key NOT NULL,
+#                           lessons text NOT NULL,
+#                           description text NOT NULL,
+#                           deadline date NOT NULL,
+#                           format text NOT NULL,
+#                           image_date bytea NOT NULL)'''
+#     cursor.execute(create_table_query)
+#     connection.commit()
+#     print("Таблица была создана наверное)")
+#     # Вывод сведений о базе данных
+#     print("Информация о сервере базы данных...")
+#     print(connection.get_dsn_parameters(), "\n")
+#     cursor.execute("SELECT version();")
+#     record = cursor.fetchone()
+#     print("Вы подключены к - ", record, "\n")
+# except (Exception, p2.Error) as error:
+#     print("Ошибка при работе с базой данных:", error)
+# finally:
+#     if connection:
+#         cursor.close()
+#         connection.close()
+#         print("Соединение с базой данных прервано...")
