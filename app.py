24cf08787d3c910f406699e13433b14d9c23515b
Cristalical <ak3146753@gmail.com>
2024-10-17 21:15:44 +0700

    ono rabotaet!!!!!!!!!!!!!!!!

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from aiogram import types
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from aiogram import F, Router
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, Message, InlineKeyboardButton, InlineKeyboardMarkup
 from aiogram.filters import Command
 import asyncio
 from loader import dp, bot
+start_router = Router()
+
 # from handlers.user.menu import admin_menu, user_menu
 user_message = "Челик"
 admin_message = "БОГ!"
@@ -31,18 +34,32 @@ def main_kb(user_telegram_id: int):
     keyboard = ReplyKeyboardMarkup(keyboard=kb_list, resize_keyboard=True, one_time_keyboard=True)
     return keyboard
 
+def ease_link_kb():
+    inline_kb_list = [
+        [InlineKeyboardButton(text="ОДК", url='https://habr.com/ru/users/yakvenalex/')],
+        [InlineKeyboardButton(text="Мой Telegram", url='tg://resolve?domain=yakvenalexx')]
+    ]
+    return InlineKeyboardMarkup(inline_keyboard=inline_kb_list)
+
 @dp.message(Command("menu"))
 async def cmd_start(message: types.Message):
 
-    await message.answer('''а вот тут уже должны быть кнопки с предметами''')
+    await message.answer('''Выбери предмет, сучка)''', reply_markup=ease_link_kb())
 
 @dp.message(Command("sos"))
 async def cmd_start(message: types.Message):
 
-    await message.answer('''айдишник кристаликал и невероятнейше''')
+    await message.answer('''Обращайтесь к ебейшему @cristalical
+                            и его рабу @neveroyatneyshee!!!''')
 
 async def main():
     await dp.start_polling(bot)
+
+@dp.message(F.text == 'ОДК')
+async def cmd_start(message: types.Message):
+    await message.answer('''ТЫ ДАУН АХАХАХАХХА''', reply_markup=types.ReplyKeyboardRemove())
+
+
 
 if __name__ == "__main__":
     asyncio.run(main())
