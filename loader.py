2e59d4275420f7c43d73579fe799a44ac5cb1839
snhbmp <I7o4Ta1290@gmail.com>
2024-10-31 13:39:39 +0300

    sosat' america

--- loader.py
+++ loader.py
@@ -4,11 +4,11 @@ from aiogram.enums.parse_mode import ParseMode
 from aiogram.client.default import DefaultBotProperties
 
 from data.config import BOT_TOKEN
-from utils.db.storage import DatabaseManager
+from utils.db.storage import DatebaseManager
 
 # '''Тут я сделал так, чтобы бот мог работать с html кодом'''
 bot = Bot(token=BOT_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
 storage = MemoryStorage()
 dp = Dispatcher(storage=storage)
-db = DatabaseManager()
+db = DatebaseManager()
 print(db.fetchall(f"SELECT deadline FROM all_homework WHERE lessons = \'init\'"))
--- utils/db/storage.py
+++ utils/db/storage.py
@@ -1,82 +1,52 @@
 import psycopg2 as p2
 
-class DatabaseManager:
+class DatebaseManager:
     def __init__(self):
         try:
-            self.info_db = {
-                "user": "postgres",
-                "password": "qwerty",
-                "host": "127.0.0.1",
-                "port": "5432",
-                "database": "dz_bot_bd"
-            }
+            self.info_db = {"user": "postgres",
+                            "password": "qwerty",
+                            "host": "127.0.0.1",
+                            "port": "5432",
+                            "database": "dz_bot_bd"}
             self.conn = p2.connect(**self.info_db)
             self.cur = self.conn.cursor()
             print("Ура")
-        except p2.Error as e:
-            print("Error:", e)
+        except:
+            print("error")
 
     def create_db(self):
-        try:
-            conn = p2.connect(user="postgres", password="qwerty", host="127.0.0.1", port="5432", database="postgres")
-            conn.autocommit = True
-            cur = conn.cursor()
-            cur.execute("CREATE DATABASE dz_bot_bd")
-            cur.close()
-            conn.close()
-            print("Database created successfully")
-        except p2.Error as e:
-            print("Error creating database:", e)
+        self.query("CREATE DATABASE dz_bot_bd")
 
     def create_table(self):
-        self.query('''CREATE TABLE IF NOT EXISTS all_homework
-                    (all_homework_id SERIAL PRIMARY KEY,
-                    lessons TEXT NOT NULL,
-                    description TEXT NOT NULL,
-                    deadline DATE NOT NULL,
-                    format TEXT NOT NULL,
-                    image_date TEXT NOT NULL)''')
+        self.query('''CREATE TABLE IF NOT EXISTS all_homework 
+                    (all_homework_id integer primary key NOT NULL,
+                    lessons text NOT NULL,
+                    description text NOT NULL,
+                    deadline data NOT NULL,
+                    format text NOT NULL,
+                    image_date bytea NOT NULL)''')
 
     def query(self, arg, values=None):
-        try:
-            if values is None:
-                self.cur.execute(arg)
-            else:
-                self.cur.execute(arg, values)
-            self.conn.commit()
-        except p2.Error as e:
-            print("Error executing query:", e)
+        if values in None:
+            self.cur.execute(arg)
+        else:
+            self.cur.execute(arg, values)
+        self.conn.commit()
 
     def fetchone(self, arg, values=None):
-        try:
-            if values is None:
-                self.cur.execute(arg)
-            else:
-                self.cur.execute(arg, values)
-            return self.cur.fetchone()
-        except p2.Error as e:
-            print("Error fetching one:", e)
+        if values is None:
+            self.cur.execute(arg)
+        else:
+            self.cur.execute(arg, values)
+        return self.cur.fetchone()
 
     def fetchall(self, arg, values=None):
-        try:
-            if values is None:
-                self.cur.execute(arg)
-            else:
-                self.cur.execute(arg, values)
-            return self.cur.fetchall()
-        except p2.Error as e:
-            print("Error fetching all:", e)
+        if values is None:
+            self.cur.execute(arg)
+        else:
+            self.cur.execute(arg, values)
+        return self.cur.fetchall()
 
     def close(self):
-        try:
-            self.cur.close()
-            self.conn.close()
-            print("Connection closed")
-        except p2.Error as e:
-            print("Error closing connection:", e)
-
-# Usage
-# b = DatabaseManager()
-# b.create_db()
-# b.create_table()
-# b.close()
+        self.conn.close()
+        self.cur.close()
